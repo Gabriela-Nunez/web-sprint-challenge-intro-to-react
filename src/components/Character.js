d2fd6f1320168c7b091079446b54557f83fc88c1
Gabby Nunez <gabbynunez@Gabbys-MacBook-Pro.local>
2022-11-01 21:16:55 -0500

    MVP tasks were met

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,2 +1,29 @@
 // Write your Character component here
-import react from 'React';
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledCharacter = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  color: "sienna";
+
+`;
+
+const Character = (props) => {
+
+  const { character } = props;
+
+  return (
+    <StyledCharacter>
+      <h2>{character.name}</h2>
+      <p>{character.birth_year}</p>
+      <p>{character.gender}</p>
+      <p>{character.hair_color}</p>
+      <p>{character.eye_color}</p>
+    </StyledCharacter>
+  )
+}
+
+
+export default Character;
